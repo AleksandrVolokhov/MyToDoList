a295a4a752f4ff9459c745dbb9bc65296d42925b
AleksandrVolokhov <90680366+AleksandrVolokhov@users.noreply.github.com>
2023-08-09 15:15:52 +0200

    Added an empty project

---
+++ PlanPro/PlanPro/PlanPro/PlanProApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanProApp.swift
+//  PlanPro
+//
+//  Created by Alexander Volokhov on 09.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct PlanProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
